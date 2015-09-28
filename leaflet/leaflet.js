0dae1d6219038953c17fc17f5988bc868a1a3aa5
carriesmith <carrie.elizabeth@gmail.com>
2015-09-28 18:20:16 -0400

    test

---
+++ leaflet/leaflet.js
@@ -0,0 +1,201 @@
+
+var data = [
+  {
+    "start_date": "09/26/2015",
+    "end_date": "09/26/2015",
+    "start_time": "11:00:00",
+    "end_time": "17:00:00",
+    "repeat": 0,
+    "sat": 0,
+    "sun": 0,
+    "mon": 0,
+    "tue": 0,
+    "wed": 0,
+    "thur": 0,
+    "fri": 0,
+    "title": "pagan pride harvest fest",
+    "descriptionShort": "Annual Festival with vendors, workshops, speakers, open ritual, performers.",
+    "description": "NYC-PPD is proud to present their 15th annual NY Pagan Pride Harvest Festival, a communal celebration of all things Pagan! Join us on 9/26/15 in Washington Square Park to meet Pagans, Wiccans, Druids, Voodoo practitioners, Thelemics, and others as we participate in multi-denominational rituals and workshops. Browse vendor booths and buy marvelous magical merchandise of all kinds (jewelry, ritual items and tools, clothes, books, etc.), experience tarot and other readings, join in a drum circle, and visit information tables to learn about the variety of Pagan groups that meet in our city. All Pagan-friendly open-minded folk are welcome!",
+    "link": "https://www.facebook.com/events/982557818441842/",
+    "price": 0,
+    "suggestedDonation": 0,
+    "location": "washington square park",
+    "address": 0,
+    "lat": 40.7308228,
+    "long": -73.997332
+  },
+  {
+    "start_date": "09/26/2015",
+    "end_date": "09/26/2015",
+    "start_time": "11:30:00",
+    "end_time": "14:00:00",
+    "repeat": 0,
+    "sat": 0,
+    "sun": 0,
+    "mon": 0,
+    "tue": 0,
+    "wed": 0,
+    "thur": 0,
+    "fri": 0,
+    "title": "the punk rope games",
+    "descriptionShort": "Double-dutch takes a whimsical turn at the punk rope games, now in their seventh year. watch contestants don sartorial dress and play to win in absurd, jump rope-inspired games at greenpoint’s newtown barge playground.",
+    "description": "The Punk Rope Games were founded in 2009 and take place annually in New York City. The Games feature teams of 4 in costume competing in 10 fun and athletic events ranging from the sublime to the ridiculous. There are 4 individual, 2 partner, and 4 team events. Participants of all fitness levels are welcome. Space is limited to 16 teams.",
+    "link": "http://punkrope.com/punk-rope-games/",
+    "price": 10,
+    "suggestedDonation": 1,
+    "location": "Newtown Barge Playground",
+    "address": "Commercial & DuPont Streets",
+    "lat": 40.7359336,
+    "long": -73.960045
+  },
+  {
+    "start_date": "09/30/2015",
+    "end_date": "09/30/2015",
+    "start_time": "21:00:00",
+    "end_time": 0,
+    "repeat": 0,
+    "sat": 0,
+    "sun": 0,
+    "mon": 0,
+    "tue": 0,
+    "wed": 0,
+    "thur": 0,
+    "fri": 0,
+    "title": "twin peaks bingo",
+    "descriptionShort": "What better way to celebrate this groundbreaking show than to gather with fellow Twin Peaks lovers and play a damn fine game of bingo!",
+    "description": "Back in 1990 Rolling Stone called it “like nothing else on television.”\n\n25 years later, it’s still like nothing else on television.\n\nTwin Peaks radically changed the concept of prime time TV: its serialized format, its central mystery, its weirdness.  Unfortunately, it was so ahead of its time that just like its heroine Laura Palmer, it met with an untimely end, and after just 2 seasons it was canceled.\n\nBut Videology is bringing it back! No, we’re not producing the 3rd season here at Videology (we’re leaving that to someone else), but we’re doing the next best thing: we’re watching it and playing bingo! What better way to celebrate this groundbreaking show than to gather with fellow Twin Peaks lovers and play a damn fine game of bingo!",
+    "link": "http://videologybarandcinema.com/events/twin-peaks-bingo/",
+    "price": 0,
+    "suggestedDonation": 0,
+    "location": "Videology",
+    "address": "308 Bedford Ave. Brooklyn, NY 11249",
+    "lat": 40.7138782,
+    "long": -73.9619375
+  },
+  {
+    "start_date": "10/2/2015",
+    "end_date": "10/2/2015",
+    "start_time": "23:59:00",
+    "end_time": "1:32:00",
+    "repeat": 0,
+    "sat": 0,
+    "sun": 0,
+    "mon": 0,
+    "tue": 0,
+    "wed": 0,
+    "thur": 0,
+    "fri": 0,
+    "title": "$5 Midnights - The Brood",
+    "descriptionShort": "Calling all insomniacs! If you’re looking for some late night entertainment, keep up with all our midnight screenings here!",
+    "description": "David Cronenberg’s THE BROOD is the inaugural film in our new $5 MIDNIGHTS series!\n\n“It’s Cronenberg’s Kramer vs. Kramer – although [Kramer] never featured dwarfish homicidal psychopaths amongst its methods for bridging irreconcilable differences.” Anton Bitel, Movie Gazette\n\nA man tries to uncover an unconventional psychologist’s therapy techniques on his institutionalized wife, while a series of brutal attacks committed by a brood of mutant children coincides with the husband’s investigation.\n\nOliver Reed channeling L. Ron Hubbard. Cronenberg channeling Satan.\n\nCourtesy of Janus Films!\n\nTHE BROOD, 92min, Rated R, 21+ only",
+    "link": "http://videologybarandcinema.com/events/the-brood/",
+    "price": 5,
+    "suggestedDonation": 1,
+    "location": "Videology",
+    "address": "308 Bedford Ave. Brooklyn, NY 11249",
+    "lat": 40.7138782,
+    "long": -73.9619375
+  },
+  {
+    "start_date": "09/28/2015",
+    "end_date": "09/28/2015",
+    "start_time": "21:00:00",
+    "end_time": "23:00:00",
+    "repeat": 0,
+    "sat": 0,
+    "sun": 0,
+    "mon": 0,
+    "tue": 0,
+    "wed": 0,
+    "thur": 0,
+    "fri": 0,
+    "title": "Free Rooftop Movies - Predator (1987)",
+    "descriptionShort": "Celebrate the great outdoors like a true New Yorker: in front of a big screen. To add to your fun-filled summer in the city, we bring you the skint’s 2015 Free Outdoor Summer Movies Guide!",
+    "description": 0,
+    "link": "http://www.theskint.com/guide-free-outdoor-movies-all-summer-long/",
+    "price": 0,
+    "suggestedDonation": 0,
+    "location": "our wicked lady",
+    "address": "153 Morgan Ave, New York",
+    "lat": 40.70937,
+    "long": -73.933333
+  },
+  {
+    "start_date": "10/03/2015",
+    "end_date": "10/03/2015",
+    "start_time": "10:00:00",
+    "end_time": "12:30:00",
+    "repeat": 0,
+    "sat": 0,
+    "sun": 0,
+    "mon": 0,
+    "tue": 0,
+    "wed": 0,
+    "thur": 0,
+    "fri": 0,
+    "title": "NYC Craft Coffee Festival",
+    "descriptionShort": "An immersive coffee experience when the NYC Craft Coffee Festival kicks off Saturday, October 3, 2015",
+    "description": "Get ready for an immersive coffee experience when the NYC Craft Coffee Festival kicks off Saturday, October 3, 2015. Twenty specially curated roasters and coffee shops share with you unique beans and flavor profiles with a soundtrack provided by live entertainment at Villain in Williamsburg.  This is a fun environment for true coffee enthusiasts with a strictly limited ticket capacity.  Think about your ideal coffee event experience: this is it.\n\nYou’ll have the chance to sample 20 roasters and coffee shops to your heart's content while live entertainment keeps the party spirited during this one of a kind experience.\n\nThe NYC Craft Coffee Festival is the official kickoff to NYC Coffee Week™.",
+    "link": "http://www.nyccraftcoffeefest.com/",
+    "price": 35,
+    "suggestedDonation": 0,
+    "location": "Villain",
+    "address": "50 North 3rd Street, Brooklyn, New York 11249",
+    "lat": 40.7177441,
+    "long": -73.9639422
+  },
+  {
+    "start_date": "10/03/2015",
+    "end_date": "10/03/2015",
+    "start_time": "14:00:00",
+    "end_time": "16:30:00",
+    "repeat": 0,
+    "sat": 0,
+    "sun": 0,
+    "mon": 0,
+    "tue": 0,
+    "wed": 0,
+    "thur": 0,
+    "fri": 0,
+    "title": "NYC Craft Coffee Festival",
+    "descriptionShort": "An immersive coffee experience when the NYC Craft Coffee Festival kicks off Saturday, October 3, 2015",
+    "description": "Get ready for an immersive coffee experience when the NYC Craft Coffee Festival kicks off Saturday, October 3, 2015. Twenty specially curated roasters and coffee shops share with you unique beans and flavor profiles with a soundtrack provided by live entertainment at Villain in Williamsburg.  This is a fun environment for true coffee enthusiasts with a strictly limited ticket capacity.  Think about your ideal coffee event experience: this is it.\n\nYou’ll have the chance to sample 20 roasters and coffee shops to your heart's content while live entertainment keeps the party spirited during this one of a kind experience.\n\nThe NYC Craft Coffee Festival is the official kickoff to NYC Coffee Week™.",
+    "link": "http://www.nyccraftcoffeefest.com/",
+    "price": 35,
+    "suggestedDonation": 0,
+    "location": "Villain",
+    "address": "50 North 3rd Street, Brooklyn, New York 11249",
+    "lat": 40.7177441,
+    "long": -73.9639422
+  }
+]
+
+
+var app = {};
+app.accessToken = 'pk.eyJ1IjoiY2Fycmllc21pdGgiLCJhIjoiY2lmNGRnYmF4MDF3ZHM5bTc3YTY0MzhkMSJ9.BKwqGXXsnCIAuUhc99sWng';
+app.mapboxProjectId = 'carriesmith.nii6d5je';
+
+// initialize the map
+var map = L.map('map').setView([40.7033127, -73.979681], 11);
+
+// load a tile layer
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 20,
+    minZoom: 8,
+    id: app.mapboxProjectId,
+    accessToken: app.accessToken
+}).addTo(map);
+
+for (var i in data){
+  console.log(data[i]);
+
+  var marker = L.marker([data[i].lat, data[i].long] ).addTo(map);
+  marker.bindPopup("<div class='popTitle'>" 
+    + data[i].title 
+    + "</div><div class='popDesc'>"
+    + data[i].descriptionShort
+    + "</div>"
+    );
+}
+
